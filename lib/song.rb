931021a690f5883e29adcc0171c056d5ace5ffcb
Christopher Osborn <z68ozz@gmail.com>
2020-05-16 00:17:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,8 +18,8 @@ class Song
   end
   
   def self.new_by_filename(filename)
-    artist 
-    song_name 
+    artist = filename.split(" - ")[0]
+    song_name = filename.split
     file_ext
   end
   
